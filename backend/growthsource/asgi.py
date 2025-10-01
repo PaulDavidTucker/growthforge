280733bc5fec5922792f5cb47bf85824be306b13
PaulDavidTucker <tuckerpauldavid@gmail.com>
2025-10-01 23:32:12 +0100

    Updated docs and added scaffold for chatbot

--- backend/growthsource/asgi.py
+++ backend/growthsource/asgi.py
@@ -1,16 +1,24 @@
-"""
-ASGI config for backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack  # We'll need this for user auth later
+# You will need to create the chat.routing module mentioned below
+# import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "growthsource.settings")
 
-application = get_asgi_application()
+# This is the standard Django HTTP application
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        # (HTTP requests will be handled by the normal Django views)
+        "http": django_asgi_app,
+        # (WebSocket requests will be handled by...)
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                # chat.routing.websocket_urlpatterns # 👈 This is where you'll define your WebSocket URLs
+            )
+        ),
+    }
+)
